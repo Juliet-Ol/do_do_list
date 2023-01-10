21ca9c5a7b4258b40fb2d5393be21cc27f7d3e90
Juliet <agutu@gmail.com>
2023-01-10 10:58:31 +0300

    create selectors, event listeners and functions on js

---
+++ app.js
@@ -0,0 +1,22 @@
+
+//Selectors
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+// Event Listeners
+todoButton.addEventListener('click', addTodo)
+
+
+//Functions
+function addTodo(event){
+    //prevent form from submitting
+    event.preventDefault();
+    //Todo div
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+    //Create li
+    const newTodo = document.createElement('li')
+
+    
+}
